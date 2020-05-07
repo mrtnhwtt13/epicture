84ba48f344139ea3fe467c299e1af176b5a719c4
Martin Hewitt <martin.hewitt@epitech.eu>
2020-05-07 14:34:20 +0200

    testing

--- ePicture/App.js
+++ ePicture/App.js
@@ -13,7 +13,7 @@ export default class ImagePickerExample extends React.Component {
 
   render() {
     let { image } = this.state;
-    const up_button = <Button title="Upload" />
+    const up_button = <Button title="Upload" onPress={} />
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Button title="Pick an image from camera roll" onPress={this._pickImage} />
@@ -52,22 +52,22 @@ export default class ImagePickerExample extends React.Component {
       console.log(result);
       const base64 = await FileSystem.readAsStringAsync(result.uri, { encoding: 'base64' });
       //console.log(base64)
-      uploadToImgur(base64)
+      //uploadToImgur()
     } catch (E) {
       console.log(E);
     }
   };
 }
 
-const uploadToImgur = (base64) => {
- 
+const uploadToImgur = () => {
+ console.log('hej hej')
   fetch('https://api.imgur.com/3/upload', {
     method: 'POST',
     headers: {
       'Authorization': 'Client-ID 1ea9d8b68e5bf9f'
     },
     body: JSON.stringify({
-      'image': base64
+      'image': ''
     })
   })
   .then(response => console.log(response))
