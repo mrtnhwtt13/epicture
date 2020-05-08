b914ecc80b0472c1f6c0cbdd256276f640fa648a
Sébastien CHOUBRAC <sebastien.choubrac@epitech.eu>
2020-05-08 16:51:53 +0200

    home separated & files cleaned

--- ePicture/App.js
+++ ePicture/App.js
@@ -1,22 +1,9 @@
 import * as React from 'react';
-import { View, Text, StyleSheet, TouchableHighlight } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Main from './app/Main'
+import HomeScreen from './app/Home';
+import Main from './app/Main';
 import ViewImages from './app/ViewImages';
-
-function HomeScreen({ navigation }) {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Welcome to Epicture !</Text>
-            <View style={style.buttonContainer}>
-                <TouchableHighlight underlayColor='#3f62aa' style={[ style.button ]} onPress={() => navigation.navigate('Main')}>
-                    <Text style={style.buttonText}>Start</Text>
-                </TouchableHighlight>
-            </View>
-        </View>
-    );
-}
 
 const Stack = createStackNavigator();
 
@@ -56,29 +43,6 @@ function App() {
     );
 }
 
-
 //console.disableYellowBox = true;
-
-const style = StyleSheet.create({    
-    buttonContainer: {
-        flexDirection: 'row',
-        justifyContent: 'center'
-    },
-    button: {
-        marginRight: 20,
-        marginTop: 15,
-        padding: 15,
-        paddingLeft: 30,
-        paddingRight: 30,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#3b5998',
-        borderRadius: 4
-    },
-    buttonText: {
-        color: 'white',
-        fontWeight: 'bold'
-    }
-})
 
 export default App;
---
+++ ePicture/app/Home.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { View, Text, TouchableHighlight, StyleSheet } from 'react-native';
+
+export default class HomeScreen extends React.Component {
+    render () {
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Welcome to Epicture !</Text>
+                <View style={style.buttonContainer}>
+                    <TouchableHighlight underlayColor='#3f62aa' style={[ style.button ]} onPress={() => this.props.navigation.navigate('Main')}>
+                        <Text style={style.buttonText}>Start</Text>
+                    </TouchableHighlight>
+                </View>
+            </View>
+        );
+    }
+}
+
+const style = StyleSheet.create({    
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
+    button: {
+        marginRight: 20,
+        marginTop: 15,
+        padding: 15,
+        paddingLeft: 30,
+        paddingRight: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#3b5998',
+        borderRadius: 4
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold'
+    }
+})
