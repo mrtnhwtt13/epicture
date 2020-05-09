eeb148b5fa0b3fd8df4bbb1fc6e74ee38fb97e60
Sébastien CHOUBRAC <sebastien.choubrac@epitech.eu>
2020-05-09 12:10:29 +0200

    click to favorites

--- ePicture/app/ImageCard.js
+++ ePicture/app/ImageCard.js
@@ -2,7 +2,6 @@ import React from 'react'
 import {
     View,
     Text,
-    StyleSheet,
     Image,
     Dimensions
 } from 'react-native'
@@ -13,21 +12,48 @@ let windowWidth = Dimensions.get('window').width
   
 
 export default class ImageCard extends React.Component {
-    
-    renderRow (rowData) {
-        if (rowData.images) {
-            if (rowData.images[0].link.match(/\.(jpg|png|gif)/g)) {
+
+    constructor (props) {
+        super(props)
+        this.state = {
+            favorite: false
+        }
+        this.handleAddToFav = this.handleAddToFav.bind(this);
+    }
+
+    handleAddToFav() {
+        if (this.state.favorite === false) {
+            this.setState({ favorite: true });
+            console.log(this.state.favorite)
+        }
+        else {
+            this.setState({ favorite: false });
+            console.log(this.state.favorite)
+        }
+    }   
+  
+    render () {
+        let { favorite } = this.state;
+        let favIcon;
+        if (favorite === false) {
+            favIcon = ( <Ionicons name="ios-star-outline" size={16} color="white" style={{ paddingTop: 5 }} onPress={() => this.handleAddToFav()} /> )
+        }
+        else {
+            favIcon = ( <Ionicons name="ios-star" size={16} color="yellow" style={{ paddingTop: 5 }} onPress={() => this.handleAddToFav()} /> )
+        }
+        if (this.props.result.images) {
+            if (this.props.result.images[0].link.match(/\.(jpg|png|gif)/g)) {
                 return (
                     <View style={{ flex: 1, alignItems: "center", justifyContent: "center", marginBottom: 10 }}>
                         <View style={{ backgroundColor: "#4e4f61", borderRadius: 10, overflow: "hidden" }}>
                             <View style={{ padding: 10, width: 155 }}>                                
                                 <Text style={{ color: "#fff", paddingTop: 5 }}>
-                                    { rowData.images[0].description }
+                                    { this.props.result.images[0].description }
                                 </Text>                                
                             </View>                            
                             <View>
                                 <Image
-                                    source={{ uri: rowData.images[0].link }}
+                                    source={{ uri: this.props.result.images[0].link }}
                                     style={{
                                         height: windowWidth - 10,
                                         width: windowWidth - 10
@@ -37,10 +63,10 @@ export default class ImageCard extends React.Component {
                             <View style={{ padding: 10, width: windowWidth - 10, flexDirection: 'row', justifyContent: 'space-between' }}>
                                 <View style={{ paddingTop: 5, flexDirection: 'row' }}>
                                     <Ionicons name="md-eye" size={16} color="white" style={{ marginRight: 2 }} />
-                                    <Text style={{ color: "#fff" }}>{ rowData.images[0].views }</Text>
-                                    <Text style={{ color: "#fff", marginLeft: 5, fontStyle: 'italic' }}>{new Date(rowData.images[0].datetime * 1000).toLocaleString("en-US", {year: "numeric", month: "numeric", day: "numeric"})}</Text>                                    
+                                    <Text style={{ color: "#fff" }}>{ this.props.result.images[0].views }</Text>
+                                    <Text style={{ color: "#fff", marginLeft: 5, fontStyle: 'italic' }}>{new Date(this.props.result.images[0].datetime * 1000).toLocaleString("en-US", {year: "numeric", month: "numeric", day: "numeric"})}</Text>                                    
                                 </View>
-                                <Ionicons name="ios-star-outline" size={16} color="white" style={{ paddingTop: 5 }} />                                
+                                {favIcon}                                
                             </View>
                         </View>
                     </View>                    
@@ -53,11 +79,5 @@ export default class ImageCard extends React.Component {
         else {
             return null
         }
-    }
-  
-    render () {
-        let card = this.renderRow(this.props.result)
-  
-        return card
     }
 }
