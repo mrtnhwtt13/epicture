e2436dbfe56ef5d14c60b84ad3d6e63526a8f0c9
Martin Hewitt <martin.hewitt@epitech.eu>
2020-05-09 13:35:23 +0200

    auth wip

--- ePicture/app/Connect.js
+++ ePicture/app/Connect.js
@@ -2,9 +2,16 @@ import React from 'react';
 import { Linking } from 'react-native';
 import { WebView } from 'react-native-webview'
 
+
+_handleUrl = url => {
+    this.setState({ url });
+    let { path, queryParams } = Linking.parse(url);
+    alert(`Linked to app with path: ${path} and data: ${JSON.stringify(queryParams)}`);
+};
+
 export default class Connect extends React.Component {
     render() {
-        const uri = 'https://api.imgur.com/oauth2/authorize?client_id=8129a5baf4d8e11&response_type=token&state=';
+        const uri = 'https://api.imgur.com/oauth2/authorize?client_id=1ea9d8b68e5bf9f&response_type=token&state=';
         return (
             <WebView
                 ref={(ref) => { this.webview = ref; }}
