4dd3a3116f28c34d24d257378fd90c8081881c73
Martin Hewitt <martin.hewitt@epitech.eu>
2020-05-11 13:59:58 +0200

    myimage wip

--- ePicture/app/API/apiRequests.js
+++ ePicture/app/API/apiRequests.js
@@ -12,6 +12,25 @@ export function searchByTag (tag) {
         })
 }
 
+export function UserImage() {
+    var myHeaders = new Headers();
+myHeaders.append("Authorization", "Bearer "+ token);
+
+var formdata = new FormData();
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  body: formdata,
+  redirect: 'follow'
+};
+
+fetch("https://api.imgur.com/3/account/me/images", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+}
+
 export function getUserInfos (token) {
     return fetch('https://api.imgur.com/3/account/me/settings', {
         method: 'GET',
---
+++ ePicture/app/MyImages.js
@@ -0,0 +1,120 @@
+import React from 'react'
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableHighlight,
+    ScrollView
+} from 'react-native'
+import { UserImage } from './API/apiRequests'
+import MyImageCard from './MyImageCard'
+
+export default class MyImages extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            dataSource: null,
+            loading: true
+        }
+    }
+
+    componentDidMount () {
+        UserImage()
+            .then((response) => {
+                this.setState({ dataSource: response.data.items, loading: false }) 
+            }, (error) => {
+                    console.log('error: ', error)
+                })
+    }
+
+
+    render () {
+        let { loading, dataSource } = this.state;
+        let results = null;
+
+        if (loading) {
+            results = (
+                <View style={style.loadingContainer}>
+                    <Text style={style.loading}>Loading images...</Text>
+                </View>)
+        }
+
+        if (!loading) {
+            results = dataSource && dataSource.map(el => <MyImageCard key={el.id} result={el} />)
+        }
+
+        return (
+            <View style={style.container}>
+                <View style={style.headingContainer}>
+                    <Text style={style.heading}>Search for { this.props.route.params.search }</Text>
+                </View>
+                <ScrollView style={style.mainContainer}>                    
+                    <View style={style.resultsContainer}>
+                        <Text style={style.results}>RESULTS</Text>                        
+                    </View>
+                    <View style={{flex: 1}}>
+                        <TouchableHighlight underlayColor='transparent' onPress={this._closeModal.bind(this)} style={style.closeButton}>
+                            <Text style={style.closeButtonText}>CLOSE</Text>
+                        </TouchableHighlight>
+                        <View style={{flex: 1}}>
+                            {results}
+                        </View>
+                    </View>             
+                </ScrollView>
+            </View>       
+        )
+    }
+}
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    headingContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 100,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ededed'
+    },
+    heading: {
+        fontSize: 20,
+        marginTop: 20
+    },
+    mainContainer: {
+        flex: 1
+    },
+    closeButton: {
+        height: 100,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderBottomColor: '#e6e6e6',
+        borderBottomWidth: 1
+    },
+    closeButtonText: {
+        fontSize: 16,
+        color: '#afafaf',
+        marginTop: 10
+    },
+    loadingContainer: {
+        alignItems: 'center',
+        marginTop: 160
+    },
+    loading: {
+        fontSize: 22,
+        color: '#7f7f7f'
+    },
+    results: {
+        color: '#c9c9c9',
+        textAlign: 'center',
+        fontSize: 13,
+        fontWeight: 'bold',
+        marginBottom: 17
+    },
+    resultsContainer: {
+        marginTop: 20,
+        paddingTop: 20,
+        borderTopWidth: 1,
+        borderTopColor: '#ededed'
+    }
+})
